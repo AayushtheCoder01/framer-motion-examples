de432636d552161d2ff4c3036a10d254085e4228
Sahil Verma <domainsahil007@gmail.com>
2024-03-04 06:17:16 +0530

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ScrollTextTransformSection from "./components/ScrollTextTransformSection";
+
+const App = () => {
+  return (
+    <div>
+      <ScrollTextTransformSection />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/PlaceHolderSection.tsx
@@ -0,0 +1,9 @@
+const PlaceHolderSection = () => {
+  return (
+    <div className="h-screen bg-green-600 text-center flex items-center justify-center">
+      Place Holder Section
+    </div>
+  );
+};
+
+export default PlaceHolderSection;
